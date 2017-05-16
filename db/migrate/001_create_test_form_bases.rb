6e779bc88437e356720459268c74e68617a14280
Shimoyama, Hiroyasu <shimoyama@spicelife.jp>
2017-05-16 16:03:57 +0900

    Update DB migration script to support ActiveRecord 4.2.X.

--- db/migrate/001_create_test_form_bases.rb
+++ db/migrate/001_create_test_form_bases.rb
@@ -1,10 +1,24 @@
-class CreateTestFormBases < ActiveRecord::Migration[ActiveRecord.version.version.split('.')[0..1].join('.')]
-  def change
-    create_table :test_form_bases do |t|
-      t.integer :integer_field
-      t.string :string_field
-      t.text :text_field
-      t.timestamps
+
+if Gem::Version.new(ActiveRecord.version.version) >= Gem::Version.new("5.0.0")
+  class CreateTestFormBases < ActiveRecord::Migration[ActiveRecord.version.version.split('.')[0..1].join('.')]
+    def change
+      create_table :test_form_bases do |t|
+        t.integer :integer_field
+        t.string :string_field
+        t.text :text_field
+        t.timestamps null: false
+      end
+    end
+  end
+else
+  class CreateTestFormBases < ActiveRecord::Migration
+    def change
+      create_table :test_form_bases do |t|
+        t.integer :integer_field
+        t.string :string_field
+        t.text :text_field
+        t.timestamps null: false
+      end
     end
   end
 end
